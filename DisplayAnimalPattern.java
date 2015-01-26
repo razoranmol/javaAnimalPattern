25d0fe3fe5b29e6c30c61b84039642f4cf6a80b4
razoranmol <anmol_0344@deerwalk.edu.np>
2015-01-26 13:59:15 +0545

    Animal print java
    Source cod to print an pattern

---
+++ DisplayAnimalPattern.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by RZR on 1/15/2015.
+ */
+class DisplayAnimalPattern {
+    public static void main(String args []){
+        System.out.println("          '__'");
+        System.out.println("          (\u00A9\u00A9)");
+        System.out.println("  /========\\/");
+        System.out.println(" / || %% ||");
+        System.out.println("*  ||----||");
+        System.out.println("   \u00A5\u00A5    \u00A5\u00A5");
+        System.out.println("   \"\"    \"\"");
+    }
+}
